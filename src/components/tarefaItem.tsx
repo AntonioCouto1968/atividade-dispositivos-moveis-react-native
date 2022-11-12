81ec22abd6f3225e28240c53d9be6c8335bc33cf
Gustavo Santos <gustavosisa@outlook.com>
2022-11-12 19:33:51 -0300

    refactor: listagem

--- src/components/tarefaItem.tsx
+++ src/components/tarefaItem.tsx
@@ -1,21 +1,43 @@
-import { List } from "react-native-paper";
+import { Text, Checkbox, IconButton } from "react-native-paper";
+import { StyleSheet, View } from "react-native";
 import { Tarefa } from "../model/tarefa";
 
 interface Props {
   tarefa: Tarefa;
-  changeStatus: (id: string) => void;
+  onDelete: (id: string) => void;
+  toggleDone: (id: string) => void;
 }
 
-export const TarefaItem = ({ tarefa, changeStatus }: Props) => {
+export const TarefaItem = ({ tarefa, onDelete, toggleDone }: Props) => {
   const shouldStrike = tarefa.done ? "line-through" : "none";
 
   return (
-    <List.Item
-      title={tarefa.name}
-      titleStyle={{
-        textDecorationLine: shouldStrike,
-      }}
-      onPress={() => changeStatus(tarefa.id)}
-    />
+    <View style={styles.container}>
+      <Checkbox
+        status={tarefa.done ? "checked" : "unchecked"}
+        onPress={() => toggleDone(tarefa.id)}
+      />
+
+      <Text
+        style={{
+          textDecorationLine: shouldStrike,
+          flexGrow: 2,
+        }}
+        variant="bodyMedium"
+      >
+        {tarefa.name}
+      </Text>
+
+      <IconButton icon="delete" onPress={() => onDelete(tarefa.id)} />
+    </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    padding: "2%",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+});
---
+++ src/components/tarefaLista.tsx
@@ -0,0 +1,47 @@
+import { View, StyleSheet } from "react-native";
+import { Text } from "react-native-paper";
+
+import { TarefaItem } from "@Components/tarefaItem";
+
+import { TarefaActions } from "../reducers/tarefa/types";
+import { makeSearchByName } from "../helpers/search";
+import { Tarefa } from "../model/tarefa";
+
+interface Props {
+  tarefas: Tarefa[];
+  search: string;
+  dispatch: (tarefas: TarefaActions.All) => void;
+}
+
+export const ListaTarefas = ({ search, tarefas, dispatch }: Props) => {
+  const doSearch = makeSearchByName(search);
+
+  const deleteTask = (id: string) => {
+    dispatch({ type: "REMOVE", payload: { id } });
+  };
+
+  const toggleDone = (id: string) => {
+    dispatch({ type: "TOGGLE", payload: { id } });
+  };
+
+  return (
+    <View style={styles.container}>
+      <Text variant="bodyLarge">Items</Text>
+      {tarefas.filter(doSearch).map((tarefa) => (
+        <TarefaItem
+          tarefa={tarefa}
+          key={tarefa.id}
+          onDelete={deleteTask}
+          toggleDone={toggleDone}
+        />
+      ))}
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    padding: "2%",
+    marginTop: "4%",
+  },
+});
